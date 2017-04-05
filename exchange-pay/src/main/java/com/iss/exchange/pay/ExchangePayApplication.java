6e9714d96581261cc728fcba8a017f82d6f26b0d
xbchena <xbchena@163.com>
2017-04-05 10:37:41 +0800

    a

---
+++ exchange-pay/src/main/java/com/iss/exchange/pay/ExchangePayApplication.java
@@ -0,0 +1,10 @@
+package com.iss.exchange.pay;
+
+public class ExchangePayApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1");
+	}
+
+}
